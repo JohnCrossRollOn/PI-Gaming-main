4aba1a733d288772998d7d9ebb0f210b3ccebc75
JohnCrossRollOn <juanrolon54@outlook.com>
2022-12-08 17:13:43 -0300

    bugfix

--- api/src/db.js
+++ api/src/db.js
@@ -9,7 +9,14 @@ const sequelize = new Sequelize(
   {
     logging: false, // set to console.log to see the raw SQL queries
     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+    dialect: "postgres",
     dialectModule: require("pg"),
+    ssl: true,
+    dialectOptions: {
+      ssl: {
+        require: true,
+      },
+    },
   }
 );
 const basename = path.basename(__filename);
